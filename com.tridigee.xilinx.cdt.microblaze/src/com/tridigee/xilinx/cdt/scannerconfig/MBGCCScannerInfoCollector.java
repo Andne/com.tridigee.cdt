297b886cce9101ea345b40af99d8cefe3b7a7bda
Andy Neebel <andy@andne.net>
2016-10-25 23:01:19 -0500

    Added the scanner collectors

---
+++ com.tridigee.xilinx.cdt.microblaze/src/com/tridigee/xilinx/cdt/scannerconfig/MBGCCScannerInfoCollector.java
@@ -0,0 +1,10 @@
+package com.tridigee.xilinx.cdt.scannerconfig;
+
+import org.eclipse.cdt.make.core.scannerconfig.IScannerInfoCollector3;
+import org.eclipse.cdt.make.internal.core.scannerconfig2.PerProjectSICollector;
+import org.eclipse.cdt.managedbuilder.scannerconfig.IManagedScannerInfoCollector;
+
+public class MBGCCScannerInfoCollector extends PerProjectSICollector
+		implements IScannerInfoCollector3, IManagedScannerInfoCollector {
+
+}
